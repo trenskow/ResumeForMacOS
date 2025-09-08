6bbabf92c05d4ff000cb568dc9d0a11a5456319d
Kristian Trenskow <this.is@kristians.email>
2025-09-08 16:25:07 +0200

    Initial Commit

---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Kristian Trenskow on 08/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
